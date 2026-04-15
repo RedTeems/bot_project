7f7344d6d95665ab0d1bb0c636d34562d2f8c016
ReDfLay <gusevandrey726@gmail.com>
2026-04-15 18:01:18 +0300

    add registration

---
+++ utils.py
@@ -0,0 +1,13 @@
+user = {
+    'first_name' : None,
+    'second_name' : None,
+    'age' : None,
+    'email' : None,
+    'address' : {
+        'city' : None,
+        'street' : None,
+        'house' : None,
+        'flat' : None
+    }
+
+}
